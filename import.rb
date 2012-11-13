fd78fc742a78cbcde6ea1e6a24cde0df9695da86
Jeroen Jacobs <git@jeroenj.be>
2012-11-13 21:47:23 +0100

    Uses icalendar to generate events
    This fixes some timezone related issues

--- import.rb
+++ import.rb
@@ -1,14 +1,14 @@
+require 'icalendar'
 require 'nokogiri'
 require 'open-uri'
+
+include Icalendar
 
 url = 'http://www.kaagent.be/wedstrijdkalender'
 doc = Nokogiri::HTML open(url)
 days = 60 * 60 * 24
 
-`echo "BEGIN:VCALENDAR
-X-WR-TIMEZONE:Europe/Brussels
-CALSCALE:GREGORIAN
-VERSION:2.0" > calendar.ics`
+calendar = Calendar.new
 
 doc.css('.datatable ul li table tr').each_with_index do |tr, i|
   from = tr.css('td')[1].content.strip
@@ -22,17 +22,15 @@ doc.css('.datatable ul li table tr').each_with_index do |tr, i|
   end
   uid = "#{from[0]}#{to[0]}#{(1..5).include?(date_time.wday) ? 'W' : 'WE'}#{uid_date.strftime('%Y%m%d')}"
 
-  # start_time - 2 hours because of our GMT+2 zone (gmt+2 because of summer time)
-  start_string = (date_time - (2 * 60 * 60)).strftime('%Y%m%dT%H%M%SZ')
-  end_string = date_time.strftime('%Y%m%dT%H%M%SZ')
-
-`echo "BEGIN:VEVENT
-UID:#{uid}
-DTSTART;VALUE=DATE-TIME:#{start_string}
-DTEND;VALUE=DATE-TIME:#{end_string}
-SUMMARY:#{from} - #{to}
-LOCATION:#{from}
-END:VEVENT" >> calendar.ics`
+  calendar.event do
+    uid uid
+    dtstart date_time.to_datetime
+    dtend (date_time + (2 * 60 * 60)).to_datetime
+    summary "#{from} - #{to}"
+    location from
+  end
 end
 
-`echo "END:VCALENDAR" >> calendar.ics`
+file = File.new 'calendar.ics', 'w'
+file.write calendar.to_ical
+file.close
